58ff15e21ca2ae2cd947665049093bb2ca1a31dc
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-09 11:07:22 +0100

    feature: Search Component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  //Set term to be default, avoids errors for empty srsearch param in API call
+  const [term, setTerm] = useState('javascript');
+
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (debouncedTerm) {
+      search();
+    }
+  }, [debouncedTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            GO
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+
+          {/*Only use when sure the data return is from a reliable source*/}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -15,6 +73,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
