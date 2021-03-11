bfbe7c4f6525583493285cea859389d8e32f17ba
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-11 12:28:01 +0100

    Move iterable objects to utils folder

---
+++ src/utils/items.js
@@ -0,0 +1,13 @@
+const items = [
+  { title: 'What is react', content: 'React is a Javascript framework' },
+  {
+    title: 'Why is React',
+    content: 'React is a favourite JS library among FrontEnd engineers',
+  },
+  {
+    title: 'How do you use React',
+    content: 'You use React by creating components',
+  },
+];
+
+export default items;
