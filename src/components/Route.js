5e05e80c4df2cced332dd6f547d7209175735e47
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-11 09:38:06 +0100

    Commented Out previous routing solution

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-const Route = (props) => {
-  const { path, children } = props;
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+// const Route = (props) => {
+//   const { path, children } = props;
+//   const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname);
-    };
-    window.addEventListener('popstate', onLocationChange);
+//   useEffect(() => {
+//     const onLocationChange = () => {
+//       setCurrentPath(window.location.pathname);
+//     };
+//     window.addEventListener('popstate', onLocationChange);
 
-    return () => {
-      window.removeEventListener('popstate', onLocationChange);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener('popstate', onLocationChange);
+//     };
+//   }, []);
 
-  return currentPath === path ? children : null;
-};
+//   return currentPath === path ? children : null;
+// };
 
-export default Route;
+// export default Route;
