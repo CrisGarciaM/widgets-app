b084134d4a17a3f7f2f8ee091bb6d3412edf0fe0
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-11 12:44:42 +0100

    Change content of items for Accordion Component

--- src/utils/items.js
+++ src/utils/items.js
@@ -1,12 +1,18 @@
 const items = [
-  { title: 'What is react', content: 'React is a Javascript framework' },
   {
-    title: 'Why is React',
-    content: 'React is a favourite JS library among FrontEnd engineers',
+    title: 'What is React',
+    content:
+      'React (also known as React.js or ReactJS) is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies.React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing. React Router is an example of such a library. And also large and complicated applications are required to make AJAX calls to fetch or mutate data. You can use any library you like with React such as Axios, JQuery AJAX or the browser built-in window.fetch.',
   },
   {
-    title: 'How do you use React',
-    content: 'You use React by creating components',
+    title: 'Why do we use React',
+    content:
+      'React allows developers to create large web applications that can change data, without reloading the page. The main purpose of React is to be fast, scalable, and simple. It works only on user interfaces in the application',
+  },
+  {
+    title: 'What is React Virtual DOM',
+    content:
+      'Another notable feature is the use of a virtual Document Object Model, or virtual DOM. React creates an in-memory data-structure cache, computes the resulting differences, and then updates the browsers displayed DOM efficiently. This process is called reconciliation. This allows the programmer to write code as if the entire page is rendered on each change, while the React libraries only render subcomponents that actually change. This selective rendering provides a major performance boost. It saves the effort of recalculating the CSS style, layout for the page and rendering for the entire page.',
   },
 ];
 
